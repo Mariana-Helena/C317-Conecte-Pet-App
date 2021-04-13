3cfd346a2a43af34d647ceb4fa8a734d08f0a48f
Mariana Helena <marianahines@hotmail.com>
2021-04-13 18:56:36 -0300

    doing pets page

--- lib/main.dart
+++ lib/main.dart
@@ -33,45 +33,76 @@ class _LoginDemoState extends State<LoginDemo> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("Login Page"),
-      ),
-      body: SingleChildScrollView(
+      body:
+      Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/Wallpaper.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(top: 60.0),
+              padding: const EdgeInsets.only(top: 100.0),
               child: Center(
                 child: Container(
                     width: 200,
-                    height: 150,
-                    /*decoration: BoxDecoration(
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(50.0)),*/
-                    child: Image.asset('images/logo.png')),
+                    height: 200,
+                    child: Image.asset('images/logopng.png')),
               ),
+            ),
+            SizedBox(
+              height: 30,
             ),
             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-              padding: EdgeInsets.symmetric(horizontal: 15),
+              padding: const EdgeInsets.only(
+                  left: 25, right: 25, top: 10, bottom: 0),
               child: TextField(
+                  style: TextStyle(
+                      height: 0.75,
+                  ),
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                    fillColor: Color.fromRGBO(255, 255, 255, 0.85),
+                    filled: true,
                     labelText: 'Email',
-                    hintText: 'Enter valid email id as abc@gmail.com'),
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    )),
               ),
             ),
             Padding(
               padding: const EdgeInsets.only(
-                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+                  left: 25, right: 25, top: 10, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
               child: TextField(
-
+                style: TextStyle(
+                  height: 0.75,
+                ),
                 obscureText: true,
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Enter secure password'),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(0),
+                      borderSide: BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                    fillColor: Color.fromRGBO(255, 255, 255, 0.85),
+                    filled: true,
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    )),
               ),
             ),
             FlatButton(
@@ -79,32 +110,53 @@ class _LoginDemoState extends State<LoginDemo> {
                 //TODO FORGOT PASSWORD SCREEN GOES HERE
               },
               child: Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
+                'Esqueceu a senha?',
+                style: TextStyle(color: Colors.white, fontSize: 15),
               ),
             ),
             Container(
               height: 50,
-              width: 250,
+              width: 310,
               decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+
+                  border: Border.all(color: Colors.white),
+                  color: Color.fromRGBO(28, 88, 124, 1)),
               child: FlatButton(
                 onPressed: () {
                   Navigator.push(
                       context, MaterialPageRoute(builder: (_) => Pets()));
                 },
                 child: Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  'Entrar',
+                  style: TextStyle(color: Colors.white, fontSize: 15),
                 ),
               ),
             ),
             SizedBox(
-              height: 130,
+              height: 10,
             ),
-            Text('New User? Create Account')
+            Container(
+              height: 50,
+              width: 310,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Color.fromRGBO(60, 72, 88, 1)),
+              child: FlatButton(
+                onPressed: () {
+
+                },
+                child: Text(
+                  'Cadastrar', style: TextStyle(color: Colors.white, fontSize: 15),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+            ),
+
           ],
         ),
+      ),
       ),
     );
   }
--- lib/pets.dart
+++ lib/pets.dart
@@ -6,37 +6,99 @@ class Pets extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MeuAppScaffold(
-        body: HomePage());
+        body: PetsPage());
   }
 }
 
-class HomePage extends StatefulWidget {
+class PetsPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState(){
-    return HomePageState();
+    return PetsPageState();
   }
 }
 
-class HomePageState extends State<HomePage>{
-  int counter = 0;
+class PetsPageState extends State<PetsPage>{
+  final pets = ['1','2','3','4'];
   @override
   Widget build(BuildContext context){
-    return Container(
-        child: Center (
-            child: GestureDetector(
-                child: Text('pets',
-                    textDirection: TextDirection.ltr,
-                    style: TextStyle(color: Colors.green, fontSize: 50)
-                ),
-                onTap: (){
-                  setState(() {
-                    counter++;
-                  });
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body:
+      SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 10,
+              ),
+              Row(
 
-                }
-            )
-        )
+                children: [
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Text('Pets cadastrados',
+
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(color: Color.fromRGBO(28, 88, 124, 1),
+                        fontSize: 28, fontWeight: FontWeight.bold)
+                ),]
+
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: <Widget>[
+                      for(var item in pets )
+                        Padding(
+                          padding: const EdgeInsets.only(top: 10.0),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  shape: BoxShape.circle
+                              ),
+                              ),
+
+                        ),
+                    ],
+                  ),
+
+
+              Container(
+                height: 60,
+                width: 250,
+                decoration: BoxDecoration(
+
+                    border: Border.all(color: Colors.white),
+                    color: Color.fromRGBO(28, 88, 124, 1)),
+                child: FlatButton(
+                  onPressed: () {
+                    /*Navigator.push(
+                        context, MaterialPageRoute(builder: (_) => PetsCadastro()));
+                  */
+                  },
+                  child: Text(
+                    'Cadastrar novo pet',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+
+            ],
+          ),
+        ),
+        ),
 
     );
   }
 }
+
+
