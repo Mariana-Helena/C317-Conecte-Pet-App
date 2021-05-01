0e73bb8be1d66431e0c362554f2aafcc0a439575
Mariana Helena <marianahines@hotmail.com>
2021-05-01 15:00:17 -0300

    getting pets using user email

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,8 @@ import 'cadastroUser.dart';
 import 'cadastroPet.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:developer';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:validators/validators.dart';
+//import 'dart:developer';
 
 void main() {
   runApp(MyApp());
@@ -26,8 +25,10 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => LoginDemo(),
         '/pets': (context) => Pets(),
-        '/consultas': (context) => AgendamentoConsulta(),
-        '/vacinas': (context) => VacinasRegistro(),
+        '/consultas': (context) => ConsultaPet(),
+        '/vacinas': (context) => VacinasPet(),
+        '/vacinas/registro': (context) => VacinasRegistro(),
+        '/consultas/agendamento': (context) => AgendamentoConsulta(),
         '/usuario/cadastro': (context) => CadastroUser(),
         '/pets/cadastro': (context) => CadastroPet(),
       },
@@ -54,7 +55,6 @@ class UserLogin {
   final String email;
   final String senha;
   UserLogin({this.email, this.senha});
-
   factory UserLogin.fromJson(Map<String, dynamic> json) {
     return UserLogin(
       email: json['email'],
@@ -71,8 +71,11 @@ class LoginDemo extends StatefulWidget {
 class _LoginDemoState extends State<LoginDemo> {
   final senhaController = TextEditingController();
   final emailController = TextEditingController();
-  final snackBar = SnackBar(content: Text('Email e/ou senha incorretos!'), backgroundColor: Color.fromRGBO(219, 13, 30, 1));
-  final snackBar2 = SnackBar(content: Text('Erro!'), backgroundColor: Color.fromRGBO(219, 13, 30, 1));
+  final snackBar = SnackBar(
+      content: Text('Email e/ou senha incorretos!'),
+      backgroundColor: Color.fromRGBO(219, 13, 30, 1));
+  final snackBar2 = SnackBar(
+      content: Text('Erro!'), backgroundColor: Color.fromRGBO(219, 13, 30, 1));
   final _formKey = GlobalKey<FormState>();
   Future<UserLogin> _futureLogin;
   @override
@@ -82,179 +85,185 @@ class _LoginDemoState extends State<LoginDemo> {
       body: Form(
         key: _formKey,
         child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("images/Wallpaper.jpg"),
-            fit: BoxFit.cover,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("images/Wallpaper.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 100.0),
-                child: Center(
-                  child: Container(
-                      width: 200,
-                      height: 200,
-                      child: Image.asset('images/logopng.png')),
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 100.0),
+                  child: Center(
+                    child: Container(
+                        width: 200,
+                        height: 200,
+                        child: Image.asset('images/logopng.png')),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
-                padding: const EdgeInsets.only(
-                    left: 25, right: 25, top: 10, bottom: 0),
-                child: TextFormField(
-                  validator: (_){
-                    Pattern pattern =
-                        r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$';
-                    RegExp regex = new RegExp(pattern);
-                    if (emailController.text == null || emailController.text.isEmpty) {
-                      return 'Digite seu email!';
-                    }
-                    else if (!regex.hasMatch(emailController.text))
-                      return 'Email inválido!';
-                    else
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 10, bottom: 0),
+                  child: TextFormField(
+                    validator: (_) {
+                      Pattern pattern =
+                          r'^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$';
+                      RegExp regex = new RegExp(pattern);
+                      if (emailController.text == null ||
+                          emailController.text.isEmpty) {
+                        return 'Digite seu email!';
+                      } else if (!regex.hasMatch(emailController.text))
+                        return 'Email inválido!';
+                      else
+                        return null;
+                    },
+                    controller: emailController,
+                    style: TextStyle(
+                      height: 0.75,
+                    ),
+                    decoration: InputDecoration(
+                        errorStyle:
+                            TextStyle(fontSize: 15, color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(0),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                        fillColor: Color.fromRGBO(255, 255, 255, 0.85),
+                        filled: true,
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        )),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: 25, right: 25, top: 10, bottom: 0),
+                  //padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: TextFormField(
+                    validator: (_) {
+                      if (senhaController.text == null ||
+                          senhaController.text.isEmpty) {
+                        return 'Digite sua senha!';
+                      }
                       return null;
-                  },
-                  controller: emailController,
-                  style: TextStyle(
-                    height: 0.75,
-                  ),
-                  decoration: InputDecoration(
-                      errorStyle: TextStyle(fontSize: 15, color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
+                    },
+                    controller: senhaController,
+                    style: TextStyle(
+                      height: 0.75,
+                    ),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        errorStyle:
+                            TextStyle(fontSize: 15, color: Colors.white),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(0),
+                          borderSide: BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
                         ),
-                      ),
-                      fillColor: Color.fromRGBO(255, 255, 255, 0.85),
-                      filled: true,
-                      labelText: 'Email',
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      )),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 25, right: 25, top: 10, bottom: 0),
-                //padding: EdgeInsets.symmetric(horizontal: 15),
-                child: TextFormField(
-                  validator: (_) {
-                    if (senhaController.text == null || senhaController.text.isEmpty) {
-                      return 'Digite sua senha!';
-                    }
-                    return null;
-                  },
-                  controller: senhaController,
-                  style: TextStyle(
-                    height: 0.75,
+                        fillColor: Color.fromRGBO(255, 255, 255, 0.85),
+                        filled: true,
+                        labelText: 'Senha',
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        )),
                   ),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      errorStyle: TextStyle(fontSize: 15, color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(0),
-                        borderSide: BorderSide(
-                          width: 0,
-                          style: BorderStyle.none,
-                        ),
-                      ),
-                      fillColor: Color.fromRGBO(255, 255, 255, 0.85),
-                      filled: true,
-                      labelText: 'Senha',
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      )),
                 ),
-              ),
-              FlatButton(
-                onPressed: () {
-                  //TODO FORGOT PASSWORD SCREEN GOES HERE
-                },
-                child: Text(
-                  'Esqueceu a senha?',
-                  style: TextStyle(color: Colors.white, fontSize: 15),
-                ),
-              ),
-              Container(
-                height: 50,
-                width: 310,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    color: Color.fromRGBO(28, 88, 124, 1)),
-                child: FlatButton(
+                FlatButton(
                   onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    setState(() {
-                      _futureLogin =
-                          doLogin(emailController.text, senhaController.text);
-                    });
-                  }},
+                    //TODO FORGOT PASSWORD SCREEN GOES HERE
+                  },
                   child: Text(
-                    'Entrar',
+                    'Esqueceu a senha?',
                     style: TextStyle(color: Colors.white, fontSize: 15),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Container(
-                height: 50,
-                width: 310,
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    color: Color.fromRGBO(60, 72, 88, 1)),
-                child: FlatButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => CadastroUser()));
-                  },
-                  child: Text(
-                    'Cadastrar',
-                    style: TextStyle(color: Colors.white, fontSize: 15),
+                Container(
+                  height: 50,
+                  width: 310,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      color: Color.fromRGBO(28, 88, 124, 1)),
+                  child: FlatButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        setState(() {
+                          _futureLogin = doLogin(
+                              emailController.text, senhaController.text);
+                        });
+                      }
+                    },
+                    child: Text(
+                      'Entrar',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-            ],
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 310,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      color: Color.fromRGBO(60, 72, 88, 1)),
+                  child: FlatButton(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (_) => CadastroUser()));
+                    },
+                    child: Text(
+                      'Cadastrar',
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
-    Future<UserLogin> doLogin(email, senha) async {
-      final response = await http.get(
-        Uri.parse('http://localhost:5000/login/${email}/${senha}'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        },
-      );
-
-      if (response.statusCode == 200) {
-        SharedPreferences prefs = await SharedPreferences.getInstance();
-        prefs.setString('user', response.body);
-        if (response.body == jsonEncode(<String, List>{'express': []})) {
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
-
+  Future<UserLogin> doLogin(email, senha) async {
+    final response = await http.get(
+      Uri.parse('http://localhost:5000/login/${email}/${senha}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    var express = jsonDecode(response.body)['express'];
+    var vet = express[0]['ehveterinario'];
+    if (response.statusCode == 200) {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('user', response.body);
+      if (response.body == jsonEncode(<String, List>{'express': []})) {
+        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      } else {
+        if (vet) {
+          Navigator.pushNamed(context, '/vacinas');
         } else {
           Navigator.pushNamed(context, '/pets');
         }
-        return UserLogin.fromJson(jsonDecode(response.body));
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(snackBar2);
       }
+      return UserLogin.fromJson(jsonDecode(response.body));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(snackBar2);
     }
-
-
+  }
 }
--- lib/pets.dart
+++ lib/pets.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'menu.dart';
 import 'cadastroPet.dart'; //para testar
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:developer';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Pets extends StatelessWidget {
   @override
@@ -9,7 +13,34 @@ class Pets extends StatelessWidget {
   }
 }
 
+class UserPets {
+  final String id;
+  final String nome;
+  final String especie;
+  final String raca;
+  final double idade;
+  final double peso;
+  final String sexo;
+  final String observacao;
+  UserPets({this.id, this.nome, this.especie, this.raca, this.idade, this.peso, this.sexo, this.observacao});
+
+  factory UserPets.fromJson(Map<String, dynamic> json) {
+    return UserPets(
+      id: json['_id'],
+      nome: json['nome'],
+      especie: json['especie'],
+      raca: json['raca'],
+      idade: json['idade'],
+      peso: json['peso'],
+      sexo: json['sexo'],
+      observacao: json['observacao']
+
+    );
+  }
+}
+
 class PetsPage extends StatefulWidget {
+
   @override
   State<StatefulWidget> createState() {
     return PetsPageState();
@@ -17,9 +48,24 @@ class PetsPage extends StatefulWidget {
 }
 
 class PetsPageState extends State<PetsPage> {
-  final pets = ['1', '2', '3', '4'];
+
+  var pets = [];
   bool openDialog = false;
-  String selectedPet;
+  String selectedPetId;
+  String selectedPetNome;
+  String selectedPetEspecie;
+  String selectedPetRaca;
+  String selectedPetSexo;
+  String selectedPetIdade;
+  String selectedPetPeso;
+  String selectedPetObs;
+  Future<UserPets> _futureLogin;
+
+  @override
+  void initState() {
+    _futureLogin = getPets();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,107 +90,117 @@ class PetsPageState extends State<PetsPage> {
               ]),
               openDialog
                   ? Center(
-                      child: AlertDialog(
-                        title: Text('Nome $selectedPet',
-                            style: TextStyle(
-                                color: Color.fromRGBO(28, 88, 124, 1),
-                                fontSize: 25,
-                                fontWeight: FontWeight.bold)),
-                        content: SingleChildScrollView(
-                          child: ListBody(
-                            children: <Widget>[
-                              Text('Espécie:', style: TextStyle(fontSize: 18)),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Raça:', style: TextStyle(fontSize: 18)),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Sexo:', style: TextStyle(fontSize: 18)),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Idade:', style: TextStyle(fontSize: 18)),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Peso:', style: TextStyle(fontSize: 18)),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Observações:',
-                                  style: TextStyle(fontSize: 18)),
-                            ],
-                          ),
-                        ),
-                        actions: <Widget>[
-                          TextButton(
-                            child: Text('Fechar',
-                                style: TextStyle(
-                                    color: Color.fromRGBO(28, 88, 124, 1),
-                                    fontSize: 15)),
-                            onPressed: () {
-                              setState(() {
-                                openDialog = false;
-                                //selectedPet=null;
-                              });
-                            },
-                          ),
-                        ],
-                      ),
-                    )
-                  : GridView.count(
-                      shrinkWrap: true,
-                      physics: ClampingScrollPhysics(),
-                      primary: false,
-                      padding: const EdgeInsets.all(20),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: 2,
+                child: AlertDialog(
+                  title: Text('$selectedPetNome',
+                      style: TextStyle(
+                          color: Color.fromRGBO(28, 88, 124, 1),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold)),
+                  content: SingleChildScrollView(
+                    child: ListBody(
                       children: <Widget>[
-                        for (var item in pets)
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10.0),
-                            child: GestureDetector(
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                      color: Colors.grey,
-                                      shape: BoxShape.circle),
-                                ),
-                                onTap: () {
-                                  setState(() {
-                                    openDialog = true;
-                                    selectedPet = item;
-                                  });
-                                }),
-                          ),
+                        Text('Espécie: $selectedPetEspecie', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Raça: $selectedPetRaca', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Sexo: $selectedPetSexo', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Idade: $selectedPetIdade anos', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Peso: $selectedPetPeso kg', style: TextStyle(fontSize: 18)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text('Observação: $selectedPetObs',
+                            style: TextStyle(fontSize: 18)),
                       ],
                     ),
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      child: Text('Fechar',
+                          style: TextStyle(
+                              color: Color.fromRGBO(28, 88, 124, 1),
+                              fontSize: 15)),
+                      onPressed: () {
+                        setState(() {
+                          openDialog = false;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              )
+                  : GridView.count(
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: <Widget>[
+                  for (var item in pets)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0),
+                      child: GestureDetector(
+                          child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey,
+                                shape: BoxShape.circle),
+                          ),
+                          onTap: () {
+                            setState(() {
+                              openDialog = true;
+                              if (pets.length != 0)
+                              {
+                                selectedPetId = item['_id'];
+                                selectedPetNome = item['nome'];
+                                selectedPetEspecie = item['especie'];
+                                selectedPetRaca = item['raca'];
+                                selectedPetSexo = item['sexo'];
+                                selectedPetIdade = item['idade'];
+                                selectedPetPeso = item['peso'];
+                                selectedPetObs = item['observacao'];
+                              }
+
+                            });
+                          }),
+                    ),
+                ],
+              ),
               openDialog
                   ? Container()
                   : Container(
-                      height: 60,
-                      width: 250,
-                      decoration:
-                          BoxDecoration(color: Color.fromRGBO(28, 88, 124, 1)),
-                      child: FlatButton(
-                        disabledColor: Color.fromRGBO(238, 238, 238, 1),
-                        onPressed: openDialog
-                            ? null
-                            : () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (_) => CadastroPet()));
-                                //para testar
-                              },
-                        child: Text(
-                          'Cadastrar novo pet',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                    ),
+                height: 60,
+                width: 250,
+                decoration:
+                BoxDecoration(color: Color.fromRGBO(28, 88, 124, 1)),
+                child: FlatButton(
+                  disabledColor: Color.fromRGBO(238, 238, 238, 1),
+                  onPressed: openDialog
+                      ? null
+                      : () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (_) => CadastroPet()));
+                    //para testar
+                  },
+                  child: Text(
+                    'Cadastrar novo pet',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+                ),
+              ),
               SizedBox(
                 height: 10,
               ),
@@ -153,5 +209,33 @@ class PetsPageState extends State<PetsPage> {
         ),
       ),
     );
+  }
+  Future<UserPets> getPets() async {
+    log('loading...');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var user = prefs.get('user');
+    var expressUser = jsonDecode(user)['express'];
+    var email = expressUser[0]['email'];
+    final response = await http.get(
+      Uri.parse('http://localhost:5000/pets/${email}'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+    log(response.body);
+    var express = jsonDecode(response.body)['express'];
+    pets = express;
+
+    if (response.statusCode == 200) {
+      if (response.body == jsonEncode(<String, List>{'express': []})) {
+        //encontrado
+
+      } else {
+        //não encontrado
+      }
+      return UserPets.fromJson(jsonDecode(response.body));
+    } else {
+      //ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
   }
 }
